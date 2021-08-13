7cad7a92b1679a51433a4d6ec5f88fc6d4380ead
Paul Phan <mrpaulphan@Pauls-MBP.fios-router.home>
2021-08-12 21:46:06 -0400

    First commit

---
+++ app/public/_includes/header.php
@@ -0,0 +1,11 @@
+<?php require_once 'database.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>IDM 252</title>
+</head>
+<body>
---
+++ app/public/index.php
@@ -0,0 +1,7 @@
+<?php require_once '_includes/header.php'; ?>
+<main>
+hi
+
+
+</main>
+<?php require_once '_includes/footer.php'; ?>
