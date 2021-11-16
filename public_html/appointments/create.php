12d82e97f507fb33f0303d426b684431bd03736c
Paul Phan <mrpaulphan@Pauls-MacBook-Pro.local>
2021-11-16 18:23:57 -0500

    test

--- public_html/appointments/create.php
+++ public_html/appointments/create.php
@@ -2,18 +2,69 @@
 $page_title = 'Book Appointments';
 include_once __DIR__ . '/../_global/header.php';
 
+if (isset($_POST['submit'])) {
+    // First create / get customer based on email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $service_id = $_POST['services'];
+    $user = $_POST['employee'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $status = 'pending';
+    $current_date = date('Y-m-d H:i:s');
+    $appointment_time = date('Y-m-d H:i:s', strtotime($date . ' ' . $time));
+
+
+    // Check if user is in the database already, if not create
+    // $query should look like "SELECT * FROM customers WHERE email = 'paul@mrpaulphan.com'"
+    $query .= 'SELECT * ';
+    $query .= 'FROM customers ';
+    $query .= "WHERE email = '{$email}'";
+    $result = mysqli_query($db_connection, $query);
+
+    // After we store/get the customer, we want to assign the ID to this variable down the page so we can insert it into the appointments table.
+    $user_id = '';
+    // Check if our query got a user from the DB
+    if ($result) {
+        // we got results, let's loop through that data and get the id from the customers table and assign it to the variable to use later
+        while ($row = mysqli_fetch_assoc($result)) {
+            $user_id = $row['id'];
+        }
+    } else {
+        // If there is no customer with that email in the DB, we assume that the customer doesn't exist so we add them in instead.
+        $query = 'INSERT INTO appointments (appointment_time, customer_id, user_id, status, service_id, date_created, date_updated)';
+        $query .= "VALUES ('{$appointment_time}', '{$customer_id}} ', '{$user_id}', '{$status}', '{$service_id}', '{$date_created}', '{$date_updated}')";
+
+
+        $result = mysqli_query($db_connection, $query);
+        if ($result) {
+            echo '<pre>';
+            var_dump($result);
+            echo '</pre>';
+            // Success
+            // redirect_to("somepage.php");
+            echo 'Success!';
+        } else {
+            // Return to page with error
+            die('Database query failed. ' . mysqli_error($db_connection));
+        }
+        die;
+    }
+}
 // Get Services to add to dropdown
 // Build Query
 $query = 'SELECT service.id, service.title, service.price, file.file_path ';
 $query .= 'FROM services AS service ';
 $query .= 'INNER JOIN files AS file ';
 $query .= 'ON service.file_id = file.id ';
-$db_results = mysqli_query($db_connection, $query);
+$get_services_results = mysqli_query($db_connection, $query);
 
 ?>
 <div class="container">
   <h1>Book Appointments</h1>
-  <form action="/_includes/process/create_appointment.php" method="POST">
+  <form action="" method="POST">
 
     <label for="">First Name</label>
     <input type="text" value="" name="first_name">
@@ -32,10 +83,10 @@ $db_results = mysqli_query($db_connection, $query);
 
     <label for="">Services</label>
     <?php
-    if ($db_results && $db_results->num_rows > 0) {
+    if ($get_services_results && $get_services_results->num_rows > 0) {
         echo '<select name="services" >';
         echo '<option disabled>Select a Service</option>';
-        while ($row = mysqli_fetch_assoc($db_results)) {
+        while ($row = mysqli_fetch_assoc($get_services_results)) {
             echo '<option value="'.$row['id'].'">' . $row['title'] . ' - $' .$row['price']. '</option>';
         }
         echo '</select>';
@@ -54,7 +105,7 @@ $db_results = mysqli_query($db_connection, $query);
 
     <br>
     <br>
-    <input class="btn btn-primary" type="submit">
+    <input class="btn btn-primary" name="submit" type="submit">
 
   </form>
 </div>
