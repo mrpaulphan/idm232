3fba9bd5d02c5209f36f7bcbfb11f59ae5f607cb
Paul <paul@mrpaulphan.com>
2022-10-25 19:42:23 -0400

    final

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,21 +1,47 @@
 <?php
-  // Step 1: Create Database Connection
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_pass = 'root';
-  $db_name = 'local_idm232_01';
-  $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+function site_url()
+{
+    $protocol = 'http';
+    // Checks if the server is using HTTPS
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+        $protocol = 'https';
+    }
+    // Get current domain
+    $domain = $_SERVER['HTTP_HOST'];
+    // Merge it with the current path
+    if ($domain === 'mrpaulphan.com') {
+        return $protocol . '://' . $domain . '/idm232/final';
+    } else {
+        return $protocol . '://' . $domain . '/final';
+    }
+}
 
-  // Check the connection is good with no errors
-  if (mysqli_connect_errno()) {
-      die('Database connection failed: ' .
-        mysqli_connect_error() .
-        ' (' . mysqli_connect_errno() . ')'
-      );
-  }
-  $page_title = 'Home';
-  include_once 'components/header.php';
-  ?>
+function project_path()
+{
+    $domain = $_SERVER['HTTP_HOST'];
+    if ($domain === 'mrpaulphan.com') {
+        return $_SERVER['DOCUMENT_ROOT'] . '/idm232/final';
+    } else {
+        return $_SERVER['DOCUMENT_ROOT'] . '/final';
+    }
+}
+// Step 1: Create Database Connection
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'root';
+$db_name = 'local_idm232_01';
+$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+// Check the connection is good with no errors
+if (mysqli_connect_errno()) {
+    die('Database connection failed: ' .
+      mysqli_connect_error() .
+      ' (' . mysqli_connect_errno() . ')'
+    );
+}
+$page_title = 'Home';
+include_once project_path() . '/components/header.php';
+?>
 
 <div class="mx-auto my-16 max-w-7xl px-4">
   <div class="text-center">
@@ -30,4 +56,4 @@
   </div>
 </div>
 
-<?php include_once 'components/footer.php';
+<?php include_once project_path() . '/components/footer.php';
