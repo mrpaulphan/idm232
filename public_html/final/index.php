516cc2846ac698ed574cb97922c30dc5858cd4a7
Paul <paul@mrpaulphan.com>
2022-11-01 18:27:44 -0400

    Work in progress

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once __DIR__ . '/app.php';
 $page_title = 'Home';
 include_once __DIR__ . '/_components/header.php';
@@ -17,22 +16,6 @@ include_once __DIR__ . '/_components/header.php';
       class="my-6 inline-flex items-center rounded border border-transparent bg-indigo-600 px-4 py-2 text-xs font-lg text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Book
       an Appointment</a>
   </div>
-
-  <h2>Create a user</h2>
-  <form action="<?php echo site_url(); ?>/process_users.php" method="POST">
-    <label for="">First Name</label>
-    <input class="border-black border-2" type="text" name="first_name">
-
-    <label for="">Last Name</label>
-    <input class="border-black border-2" type="text" name="last_name">
-
-    <label for="">Email</label>
-    <input class="border-black border-2" type="email" name="email">
-
-    <label for="">Phone Number</label>
-    <input class="border-black border-2" type="text" name="phone">
-    <input type="submit" value="submit">
-  </form>
 </div>
 
 
